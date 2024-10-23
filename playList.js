10dd5506143cd41475ff4357b836bd6b43a087c5
SENA <anamrome16@gmail.com>
2024-10-23 09:16:27 -0500

    Primer commit

---
+++ playList.js
@@ -0,0 +1,33 @@
+
+const playList = [
+    {
+      title: 'La Ocasion',
+      artist: 'De La Ghetto',
+      img: 'assets/foto-resena-min.jpg',
+      song: 'music/La Ocasión - De La Ghetto, Arcangel, Ozuna, Anuel Aa, Dj Luian, Mambo Kingz [Video Oficial] - Hear This Music (youtube).mp3'
+    },
+    {
+      title: 'Mi fanatica',
+      artist: 'De La Ghetto',
+      img: 'assets/channels4_profile.jpg',
+      song: 'ruta de la cancion'
+    },
+    {
+      title: 'Dices',
+      artist: 'De La Ghetto',
+      img: 'assets/de la .jpg',
+      song: 'music/De la Ghetto - Dices [Official Video] - De La Ghetto (youtube).mp3'
+    },
+    {
+      title: 'Sensacion Del Bloque',
+      artist: 'De La Ghetto',
+      img: 'assets/photo-1663970206579-c157cba7edda.jfif',
+      song: 'music/Sensacion Del Bloque - De La Ghetto (youtube).mp3'
+    },
+    {
+      title: 'La Groupie',
+      artist: 'De La Ghetto',
+      img: 'assets/de-la-ghetto-entrevista-espana-2023-5.jpg',
+      song: 'music/De La Ghetto - La Groupie ft. Ñejo, Luigi 21+, Nicky Jam & Ñengo Flow [Official Audio] - De La Ghetto (youtube).mp3'
+    },
+  ]
